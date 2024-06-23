f8a21bb229a57e4d2e21258a98c4ffb7c1f2a409
louisb0 <louisbaragwanath@outlook.com>
2024-06-23 16:39:10 +1200

    Remove redundant test, formatting

--- test/test_stack.c
+++ test/test_stack.c
@@ -13,7 +13,7 @@ Test(stack, stack_create) {
 Test(stack, stack_push_pop) {
   Stack s = stack_create();
 
-  stack_push(s, (void*)1);
+  stack_push(s, (void *)1);
   cr_assert_not(stack_is_empty(s));
   cr_assert_eq((int *)stack_pop(s), 1);
   cr_assert(stack_is_empty(s));
@@ -24,23 +24,8 @@ Test(stack, stack_push_pop) {
 Test(stack, stack_free) {
   Stack s = stack_create();
 
-  stack_push(s, (void*)1);
+  stack_push(s, (void *)1);
   stack_free(&s);
 
   cr_assert_null(s);
 }
-
-// Test(stack, is_empty) {
-//     Stack s = stack_create();
-//     cr_assert(stack_is_empty(s), "Expected new stack to be empty");
-//
-//     int data = 42;
-//     stack_push(s, &data);
-//     cr_assert_not(stack_is_empty(s), "Expected stack not to be empty after
-//     push");
-//
-//     stack_pop(s);
-//     cr_assert(stack_is_empty(s), "Expected stack to be empty after pop");
-//
-//     stack_free(&s);
-// }

1419ea0cb88215a0f94054efd5b304503781c60d
louisb0 <louisbaragwanath@outlook.com>
2024-06-25 16:56:05 +1200

    Align stack interface arguments

--- include/stack.h
+++ include/stack.h
@@ -3,10 +3,10 @@
 
 typedef struct Stack_T *Stack;
 
-Stack   stack_create();
+Stack   stack_create  ();
 int     stack_is_empty(Stack s);
-void    stack_push(Stack s, void *data);
-void   *stack_pop(Stack s);
-void    stack_free(Stack *s);
+void    stack_push    (Stack s, void *data);
+void   *stack_pop     (Stack s);
+void    stack_free    (Stack *s);
 
 #endif // STACK_H
